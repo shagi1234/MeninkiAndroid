ee184bd5547230fc7aa183d8d05ab30ab9f7bb60
unknown <mobdevoloper1409@gmail.com>
2023-07-12 11:17:14 +0500

    added advertisementdto

---
+++ app/src/main/java/tm/store/meninki/data/AdvertisementDto.java
@@ -0,0 +1,17 @@
+package tm.store.meninki.data;
+
+public class AdvertisementDto {
+    private String title;
+    private String description;
+    private String[] images;
+    private int id;
+    private String price;
+    private String createdAt;
+    private String category;
+    private String phoneNumber;
+    private String userName;
+    private String userAvatar;
+    private int userId;
+    private boolean isBookmarked;
+
+}
